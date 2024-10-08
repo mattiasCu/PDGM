e0f488bbd85cc72f3e50f7951da8994d98e21927
cu <cusui0210@gmail.com>
2024-10-08 18:55:37 +0200

    sss

--- graph/graph_rewiring.py
+++ graph/graph_rewiring.py
@@ -15,6 +15,28 @@ class GraphRewiring(nn.Module, core.Configurable):
         super(GraphRewiring, self).__init__()
         
         self.edge_feature = edge_feature
+        
+    
+    def adjacency_matrix_to_edge_list(self, A):
+        # 找到非零元素的索引
+        idx = torch.nonzero(A, as_tuple=False) 
+        i = idx[:, 0]  
+        j = idx[:, 1]
+        line_num = A.size(0)  
+        
+        # 计算关系编号（relation）和调整后的列索引（j_prime）
+        relation = torch.div(j, line_num, rounding_mode='floor')  # 关系编号，0到6
+        j_prime = j % line_num  # 调整后的列索引，0到184
+        
+        # 组合得到edge_list矩阵，形状为[n, 3]
+        edge_list = torch.stack([i, j_prime, relation], dim=1)
+        
+        # 根据edge_list的索引顺序，获取对应的edge_weighted矩阵
+        edge_weighted = A[i, j]
+        
+        return edge_list, edge_weighted
+
+    
 
     def edge_gearnet(self, graph, edge_list, num_relation):
         node_in, node_out, r = edge_list.t()
@@ -37,15 +59,7 @@ class GraphRewiring(nn.Module, core.Configurable):
         return graph
 
     def apply_edge_layer(self, graph, edge_list, edge_weighted):
-
-        #num_edges = edge_list.size(0)
-        #num_edges = torch.tensor(num_edges, device=graph.device)
-        #num_relations = torch.tensor(num_relations, device=graph.device)
-        #num_relation = num_relations.sum()
-        #offsets = (num_relations.cumsum(0) - num_relations).repeat_interleave(num_edges)
-        #edge_list[:, 2] += offsets
-
-        # reorder edges into a valid PackedGraph
+        
         node_in = edge_list[:, 0]
         edge2graph = graph.node2graph[node_in]
         order = edge2graph.argsort()
@@ -72,7 +86,7 @@ class GraphRewiring(nn.Module, core.Configurable):
                            view=graph.view, offsets=offsets, edge_feature=edge_feature, edge_weight=edge_weighted,
                            meta_dict=meta_dict, **data_dict)
 
-    def forward(self, graph, edge_list=None, edge_weighted = None):
+    def forward(self, graph, attn_output):
         """
         Generate a new graph based on the input graph and pre-defined node and edge layers.
 
@@ -82,6 +96,8 @@ class GraphRewiring(nn.Module, core.Configurable):
         Returns:
             graph (Graph): new graph(s)
         """
+        device = graph.device
         graph = self.apply_node_layer(graph)
-        graph = self.apply_edge_layer(graph, edge_list, edge_weighted)
+        edge_list, edge_weighted = self.adjacency_matrix_to_edge_list(attn_output)
+        graph = self.apply_edge_layer(graph, edge_list.to(device), edge_weighted.to(device))
         return graph
